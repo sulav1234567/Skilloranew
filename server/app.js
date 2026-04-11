26eaa5117515784fad854bae0100ff0fde553d6e
sulav khatiwada <sulavkhatiwada@sulavs-MacBook-Air.local>
2026-04-11 22:47:17 +0545

    issues fix

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
-
+import Dotenv from "dotenv"
+Dotenv.config()
 import express from "express"
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan"
-import Dotenv from "dotenv"
+
 import Cors from "cors"
 import AuthRouter from "./routes/Auth.routes.js"
 
 Dotenv.config()
 import connect from "./database/db.js"
-
-
 var app = express();
-connect()
-
 app.use(Cors({
    origin:[
     `${process.env.FRONTEND_URL}`
    ],
-   methods:["POST","Get"],
+   methods:["POST","GET"],
    credentials:true ,
    allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
+
+
+connect()
+
 
 app.use(logger('dev'));
 app.use(express.json());
