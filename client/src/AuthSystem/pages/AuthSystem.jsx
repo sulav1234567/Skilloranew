1b62519844adf5d0297f89c6ac5bd6f3297e2e14
sulav khatiwada <sulavkhatiwada@sulavs-MacBook-Air.local>
2026-04-12 03:31:27 +0545

    minor fix

--- client/src/AuthSystem/pages/AuthSystem.jsx
+++ client/src/AuthSystem/pages/AuthSystem.jsx
@@ -59,9 +59,12 @@ const Loginform = ({ setformtype = () => {}, closeform = () => {} }) => {
 
       try {
         let res = await api.post("/auth/login/me", formdatainp);
-        showMessages(res?.data.message, "success");
-        await getUserInfo()
-        closeform();
+        if(res?.status === 200){
+          showMessages(res?.data.message, "success");
+          await getUserInfo()
+          closeform();
+
+        }
       } catch (err) {
         if (err) {
           showMessages(err?.response?.data.message, "reject");
