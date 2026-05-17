d577b60669986e7caa263dc085296402790fe370
sulav khatiwada <sulavkhatiwada@sulavs-MacBook-Air.local>
2026-05-17 07:50:48 +0545

    dns update:

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -1,4 +1,7 @@
 import nodemailer from "nodemailer"
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
